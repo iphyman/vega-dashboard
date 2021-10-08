a99ad83cde6f19dc64d4f429e8cbd1fe910caa36
Emmanuel <opensoftweb@gmail.com>
2021-10-08 14:28:49 +0100

    update default market ID

--- src/data/constants/index.ts
+++ src/data/constants/index.ts
@@ -6,7 +6,7 @@ export {
 } from "./layouts";
 
 export const DEFAULT_MARKET_ID =
-  "9b358cb36b63001ae74b9f815c30a58f1db258fa11b17ba082a66abebed75951";
+  "f32054373b8171025f5c10bf6dc101670221ab4b690f8cbafb51bf74ed07ecde";
 
 export const VEGA_GRAPHQL_API = "https://lb.testnet.vega.xyz/query";
 export const VEGA_GRAPHQL_WS = "wss://lb.testnet.vega.xyz/query";
